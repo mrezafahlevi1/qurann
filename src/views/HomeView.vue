b01b9b73435d8d96f8ff60319484e792766b05e5
mrezafahlevi1 <12050113422@students.uin-suska.ac.id>
2022-05-08 22:06:38 +0700

    1.1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -23,6 +23,14 @@
     </div>
   </div>
   </div>
+  <div><footer class="bg-light text-center text-lg-start">
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2022 Copyright:
+    <a class="text-dark">by @mrefaz</a>
+  </div>
+  <!-- Copyright -->
+</footer></div>
 </template>
 <script>
 import axios from 'axios'
